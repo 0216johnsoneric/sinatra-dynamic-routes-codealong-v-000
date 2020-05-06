d96794632ca355d6bcb454d0929aa164c4309cd9
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-06 23:12:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,13 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   # This is a sample static route.
-  get '/hello' do
-    "Hello World!"
-  end
-
-  get '/Goodbye' do
-    "Goodbye"
-  end
+  # get '/hello' do
+  #   "Hello World!"
+  # end
+  #
+  # get '/Goodbye' do
+  #   "Goodbye"
+  # end
 
   # This is a sample dynamic route.
   get "/hello/:name" do
